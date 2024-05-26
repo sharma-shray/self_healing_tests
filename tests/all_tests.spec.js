67a8fe7d0b02609a112ced36e49f6432de7002b2
Shray Sharma <shray.sharma@phrase.com>
2024-05-26 12:58:27 +0200

    credentials updated

--- tests/all_tests.spec.js
+++ tests/all_tests.spec.js
@@ -12,9 +12,9 @@ test('Verify User Can create a workflow', async ({ page }) => {
 
     await executeDynamicCommand("Accept the cookies", page);
     await executeDynamicCommand("Verify that the Cookie banner is no longer visible", page);
-    await executeDynamicCommand("Enter text into the \"shray.sharma\" field.", page);
-    await executeDynamicCommand("Verify that \"shray.sharma\" should be visible in the Username field.", page);
-    await executeDynamicCommand("enter password \"Kidsnextdoor123!\"", page);
+    await executeDynamicCommand("Enter text into the \"shray.sharma+orchprov1@phrase.com\" field.", page);
+    await executeDynamicCommand("Verify that \"shray.sharma+orchprov1@phrase.com\" should be visible in the Username field.", page);
+    await executeDynamicCommand("enter password \"Verygoodpassword123!\"", page);
     await executeDynamicCommand("verify that the password field now has some value in it", page);
     await executeDynamicCommand("click on the login button", page);
     await executeDynamicCommand("Verify that the user should be succesfully navigated to dashboard page the url should contain ( idm-ui/dashboard)", page);
